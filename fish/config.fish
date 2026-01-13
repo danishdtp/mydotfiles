364438c7c4cce7e038b49abd63f376db1d005587
Danish Khan <danish.dtp@gmail.com>
2026-01-13 18:13:07 +0530

    updated

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,7 @@
 if status is-interactive
-	cd Documents/Data
-set -gx GREP_COLORS 'mt=01;31:fn=01;34'  # Matches in green; filenames (or addresses) in blue
-#set -gx GREP_COLOR '0;34'               # Set default matches to blue (optional)
-source dask_env/bin/activate.fish
+    cd Documents/Data
+    set -gx GREP_COLORS 'mt=01;31:fn=01;34' # Matches in green; filenames (or addresses) in blue
+    #set -gx GREP_COLOR '0;34'               # Set default matches to blue (optional)
+    source ~/Documents/Data/dask_env/bin/activate.fish
     # Commands to run in interactive sessions can go here
 end
-
-
